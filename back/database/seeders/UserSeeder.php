aacebe1daf6d02dba24ed3087fb4e24158019ec7
tara2210 <ts20200078@student.fon.bg.ac.rs>
2024-09-01 14:39:58 +0200

    Dodati factories i seeder

---
+++ back/database/seeders/UserSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+  
+    public function run(): void
+    {
+      
+        User::factory()->count(7)->create();
+        User::factory()->guest()->count(1)->create();
+    }
+}
